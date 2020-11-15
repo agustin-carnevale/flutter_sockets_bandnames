d280711945e71e1ce44ffa6598ae775bf0dcf7cc
Agustin Carnevale <acarnevale@ecointeractive.com>
2020-11-15 19:29:58 -0300

    socket bands logic

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'package:band_names/services/socket_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:band_names/models/band.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,15 +11,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Band> bands = [
-    Band(id: '1', name: 'Metallica', votes: 5),
-    Band(id: '2', name: 'Queen', votes: 1),
-    Band(id: '3', name: 'Heroes del Silencio', votes: 2),
-    Band(id: '4', name: 'Bon Jovi', votes: 7),
-  ];
+  List<Band> bands = [];
+
+  @override
+  void initState() { 
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.listen('active-bands', (bands){
+      this.bands = (bands as List).map((band) => Band.fromMap(band)).toList();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() { 
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.stopListening('active-bands');
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final socketService = Provider.of<SocketService>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -26,6 +42,14 @@ class _HomePageState extends State<HomePage> {
           'BandNames',
           style: TextStyle(color: Colors.black87),
         ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: socketService.serverStatus == ServerStatus.Online 
+            ? Icon(Icons.check_circle, color: Colors.blue[300])
+            : Icon(Icons.offline_bolt, color: Colors.red),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: bands.length,
@@ -37,12 +61,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bandTile(Band band) {
+    final socketService = Provider.of<SocketService>(context, listen: false);
     return Dismissible(
       key: Key(band.id),
       direction: DismissDirection.startToEnd,
-      onDismissed: (direction){
-        print('direction: $direction');
-        //TODO: llamar a delete en el server
+      onDismissed: (_){
+        socketService.emit('delete-band',{
+            'bandId': band.id
+          });
       },
       background: Container(
         padding: const EdgeInsets.only(left: 20),
@@ -62,7 +88,9 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 20),
         ),
         onTap: () {
-          print(band.name);
+          socketService.emit('vote-band',{
+            'bandId': band.id
+          });
         },
       ),
     );
@@ -120,9 +148,10 @@ class _HomePageState extends State<HomePage> {
 
   void addBandToList(String name) {
     if (name.length > 1) {
-      print(name);
-      this.bands.add( new Band(id: DateTime.now().toString(), name: name, votes: 0));
-      setState(() {});
+      final socketService = Provider.of<SocketService>(context, listen: false);
+      socketService.emit('add-band', {
+        'bandName': name
+      });
     }
     Navigator.pop(context); //close dialog
   }
---
+++ lib/services/socket_service.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  IO.Socket _socket;
+
+  ServerStatus get serverStatus => this._serverStatus;
+  IO.Socket get socket => this._socket;
+  Function get emit => this._socket.emit;
+  Function get listen => this._socket.on;
+  Function get stopListening => this._socket.off;
+
+  SocketService() {
+    this._initConfig();
+  }
+
+  void _initConfig() {
+    this._socket = IO.io('http://localhost:3000', {
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+
+    this._socket.on('connect', (_) {
+      this._serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });
+    this._socket.on('disconnect', (_) { 
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+    this._socket.on('message', (payload) { 
+      print('new message: $payload');
+    });
+  }
+}
