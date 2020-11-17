96c9b250cb417884f7e43f6be98f7eac007150e7
Agustin Carnevale <acarnevale@ecointeractive.com>
2020-11-17 01:14:11 -0300

    point to deployed server at heroku

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -18,7 +18,7 @@ class SocketService with ChangeNotifier {
   }
 
   void _initConfig() {
-    this._socket = IO.io('http://localhost:3000', {
+    this._socket = IO.io('https://bandnames-my-socket-server.herokuapp.com', {
       'transports': ['websocket'],
       'autoConnect': true,
     });
